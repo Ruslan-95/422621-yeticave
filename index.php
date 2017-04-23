2e5e38580b2faa11579dfae45c3de230d857903f
Ruslan <ruslan-95@bk.ru>
2017-04-23 10:34:37 +0300

    Итерация по массивам

--- index.php
+++ index.php
@@ -13,7 +13,16 @@ $now = time();
 $i = $tomorrow - $now;
 // далее нужно вычислить оставшееся время до начала следующих суток и записать его в переменную $lot_time_remaining
 $lot_time_remaining = date("h:i", mktime(0,0, $i)); // шаблон "D,d M Y h:i:s"
-?>
+
+$staffs = array('Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное');
+$ms= array(
+        array('name'=>"2014 Rossignol District Snowboard", "category" => "Доски и лыжи", "price" => 10999, "url_image" =>1),
+        array('name'=>'DC Ply Mens 2016/2017 Snowboard', 'category'=>'Доски и лыжи', 'price'=>159999, 'url_image'=>2),
+        array('name'=>'Крепления Union Contact Pro 2015 года размер L/XL', 'category'=>'Крепления', 'price'=>8000, 'url_image'=>3),
+        array('name'=>'Ботинки для сноуборда DC Mutiny Charocal', 'category'=>'Ботинки', 'price'=>10999, 'url_image'=>4),
+        array('name'=>'Куртка для сноуборда DC Mutiny Charocal', 'category'=>'Одежда', 'price'=>7500, 'url_image'=>5),
+        array('name'=>'Маска Oakley Canopy', 'category'=>'Разное', 'price'=>5400 ,'url_image'=>6)
+);?>
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -73,25 +82,32 @@ $lot_time_remaining = date("h:i", mktime(0,0, $i)); // шаблон "D,d M Y h:i
             </li>
         </ul>
     </section>
-    <section class="lots">
+    <section class="lots" >
         <div class="lots__header">
             <h2>Открытые лоты</h2>
+
             <select class="lots__select">
                 <option>Все категории</option>
+                <?php foreach ($staffs as $staff){
+                    echo "<option>$staff</option>";}
+                    unset($staff);?>
             </select>
         </div>
         <ul class="lots__list">
+            <?php foreach ($ms as $m){
+                "<li>$m</li>";
+            ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="img/lot-1.jpg" width="350" height="260" alt="Сноуборд">
+                    <img src="img/lot-<?=$m["url_image"]?>.jpg" width="350" height="260" alt="Сноуборд">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Доски и лыжи</span>
-                    <h3 class="lot__title"><a class="text-link" href="">2014 Rossignol District Snowboard</a></h3>
+                    <span class="lot__category"><?=$m['category']?></span>
+                    <h3 class="lot__title"><a class="text-link" href=""><?=$m['name']?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">10 999<b class="rub">р</b></span>
+                            <span class="lot__cost"><?=$m['price']?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             <?=$lot_time_remaining;?>
@@ -99,10 +115,10 @@ $lot_time_remaining = date("h:i", mktime(0,0, $i)); // шаблон "D,d M Y h:i
                     </div>
                 </div>
             </li>
+            <?php } unset($m);?>
         </ul>
     </section>
 </main>
-
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
