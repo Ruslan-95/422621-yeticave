4384e0048740f89ecc9696396a92503445df08f4
Ruslan-95 <ruslan-95@bk.ru>
2017-05-13 17:05:34 +0300

    #15 Аутентификация

--- add.php
+++ add.php
@@ -1,31 +1,31 @@
 <?php
+session_start();
 require_once 'functions.php';
 include_once 'data.php';
+
+if (empty($_SESSION['user'])) {
+    header('HTTP/1.1 403 Forbidden');
+    exit();
+}
 
 $errors = [];
 $file = [];
 
-
-if (isset($_POST)) {
+if (!empty($_POST)) {
     foreach ($_POST as $key => $value) {
         $_POST[$key] = htmlspecialchars($value);
 
         if (empty($value)) {
-            $errors[$key] = 'Обязательно для заполнения';
-        }
-
-        if (in_array($key, ['lot-rate', 'lot-step']) && !is_numeric($value)) {
-            $errors[$key] = 'Только цифровые значения';
+            $errors[$key] = 'Заполните это поле';
+            continue;
         }
 
         if (in_array($key, ['lot-date']) && !is_numeric($value)) {
             $errors[$key] = 'Некорректная дата';
         }
     }
-
     if (isset($_FILES['lot-file'])) {
         $file = $_FILES['lot-file'];
-
         if ($file['type'] == 'image/jpeg') {
            $uploadetfile = move_uploaded_file($file['tmp_name'], 'img/' . $file['name']);
            is_uploaded_file($uploadetfile);
@@ -33,7 +33,6 @@ if (isset($_POST)) {
             $errors['lot-file'] = 'Загрузите фото в формате jpeg';
         }
     }
-
     if (empty($errors)) {
         $lot = [
             'name' => $_POST['lot-name'],
@@ -50,7 +49,7 @@ if (isset($_POST)) {
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <title>Добавление лота</title>
+    <title>DC Ply Mens 2016/2017 Snowboard</title>
     <link href="css/normalize.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
 </head>
@@ -58,11 +57,10 @@ if (isset($_POST)) {
 
 <?= includeTemplate('header.php',[]) ?>
 <?php if (empty($_POST) || !empty($errors)): ?>
-    <?= includeTemplate('add-lot.php', ['stuff_categories' => $stuff_categories, 'errors' => $errors, 'file' => $file]) ?>
+    <?= includeTemplate('add_lot.php', ['stuff_categories' => $stuff_categories, 'errors' => $errors, 'file' => $file]) ?>
 <?php else: ?>
     <?= includeTemplate('lot_main.php', ['stuff_details' => $stuff_details, 'bets' => $bets]) ?>
 <?php endif; ?>
 <?= includeTemplate('footer.php',[]) ?>
-
 </body>
 </html>
