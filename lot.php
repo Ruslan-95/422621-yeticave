c04a69adaf38d454295c3d2d6e2e21c8ab8d5a54
Ruslan-95 <ruslan-95@bk.ru>
2017-05-16 21:33:36 +0300

    #16 Хранение данных между визитами 2.0

--- lot.php
+++ lot.php
@@ -2,26 +2,26 @@
 session_start();
 require_once 'functions.php';
 include_once 'data.php';
-$valid = is_numeric($_GET['id']) && array_key_exists($_GET['id'], $stuff_details);
-if (!$valid) {
-    header("HTTP/1.0 404 Not Found");
+
+$valid = isset($_GET['id']) ? intval($_GET['id']) : null;
+
+if (!array_key_exists($valid, $stuff_details)) {
+    header('HTTP/1.1 404 Not Found');
     exit("<h1>404… Увы, но эта страница затерялась где-то в галактике Интернета</h1>");
 }
-
-
+$new_bet = decode_array();
 if (!empty($_POST['cost'])){
-
     $new_bet[] = [
         'cost' => $_POST['cost'],
+        'lot_id' => $valid,
         'time' => time(),
-        'lot_id' => $valid
     ];
+
     $new_bet = json_encode($new_bet);
     setcookie('new_bet', $new_bet, strtotime('+30 days'));
     header( 'Location: /mylots.php');
     exit();
 }
-
 ?>
 <!DOCTYPE html>
 <html lang="ru">
