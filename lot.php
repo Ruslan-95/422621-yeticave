d8f3aa08467dd4533029c2f6fd52abc087be4983
Ruslan-95 <ruslan-95@bk.ru>
2017-05-22 01:08:16 +0300

    #16 Хранение данных между визитами 3.0

--- lot.php
+++ lot.php
@@ -3,20 +3,22 @@ session_start();
 require_once 'functions.php';
 include_once 'data.php';
 
-$valid = isset($_GET['id']) ? intval($_GET['id']) : null;
-
-if (!array_key_exists($valid, $stuff_details)) {
-    header('HTTP/1.1 404 Not Found');
+$id = isset($_GET['id']) ? intval($_GET['id']) : null;
+$product_id = $stuff_details[$id];
+if (!$id) {
+    header("HTTP/1.0 404 Not Found");
     exit("<h1>404… Увы, но эта страница затерялась где-то в галактике Интернета</h1>");
 }
 $new_bet = decode_array();
 if (!empty($_POST['cost'])){
     $new_bet[] = [
         'cost' => $_POST['cost'],
-        'lot_id' => $valid,
+        'product_id' => $product_id,
         'time' => time(),
+        'name' => $product_id['name'],
+        'category' => $product_id['category'],
+        'img' => $product_id['url_image'],
     ];
-
     $new_bet = json_encode($new_bet);
     setcookie('new_bet', $new_bet, strtotime('+30 days'));
     header( 'Location: /mylots.php');
@@ -33,7 +35,12 @@ if (!empty($_POST['cost'])){
 </head>
 <body>
 <?=includeTemplate('header.php',[]);?>
-<?=includeTemplate('lot_main.php', ['bets' => $bets, 'stuff_details'=> $stuff_details[$_GET['id']]]);?>
+<?=includeTemplate('lot_main.php', [
+    'bets' => $bets,
+    '$stuff_details'=> $stuff_details[$id],
+    'new_bet' => $new_bet,
+    'product_id' => $product_id,
+]);?>
 <?=includeTemplate('footer.php',[]);?>
 </body>
 </html>
