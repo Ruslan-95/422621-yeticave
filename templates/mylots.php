168c3f5d8aa583a92f06eba91ea0758a42ab28ad
Ruslan-95 <ruslan-95@bk.ru>
2017-05-22 01:11:01 +0300

    #16 Хранение данных между визитами 3.0

--- templates/mylots.php
+++ templates/mylots.php
@@ -1,3 +1,4 @@
+
 <main>
     <nav class="nav">
         <ul class="nav__list container">
@@ -24,35 +25,29 @@
     <section class="rates container">
         <h2>Мои ставки</h2>
         <table class="rates__list">
-            <?php foreach ($new_bet as $old) : ?>
-            <?php
-            $id = $old['lot_id'];
-            $stuff = $stuff_details[$id];
-            ?>
+            <?php foreach ($new_bet as $bet) : ?>
+
             <tr class="rates__item">
                 <td class="rates__info">
                     <div class="rates__img">
-                        <img src="<?= strip_tags($stuff['url_image'])?>" width="54" height="40" alt="Сноуборд">
+                        <img src="<?= $bet['img'] ?>" width="54" height="40" alt="Сноуборд">
                     </div>
-                    <h3 class="rates__title">
-                        <a href="lot.php?id=<?=$id?>">
-                            <?= strip_tags($stuff['name'])?>
-                        </a>
-                    </h3>
+                    <h3 class="rates__title"><a href="lot.php?id=<?=$id?>">
+                            <?= $bet['name'] ?></a></h3>
                 </td>
                 <td class="rates__category">
-                    <?=$stuff['category']?>
+                    <?= $bet['category'] ?>
                 </td>
                 <td class="rates__timer">
                     <div class="timer timer--finishing"><?=lot_time_remaining()?></div>
                 </td>
                 <td class="rates__price">
-                    <?= $old['cost']?>
+                    <?= $bet['cost'] ?>
                 </td>
                 <td class="rates__time">
-                    <?= bets_time($old['time'])?>
+                    <?= bets_time($old['time']) ?>
                 </td>
-                <?php endforeach;?>
+                <?endforeach;?>
             </tr>
         </table>
     </section>
